ac929909642dd4b6ab517be1e7f45efd467573f3
Oguz Alp Saglam <oguzalp7@github.com>
2024-08-20 17:44:28 +0300

    fix dropdown attempt1

--- src/components/dropdown.component.jsx
+++ src/components/dropdown.component.jsx
@@ -21,7 +21,7 @@ const ChakraDropdown = ({options, label, initialValue, value, onSelect}) => {
     <Box>
         <Select value={selectedValue} onChange={handleChange}>
         <option value={initialValue} label={`${label}`}><Text>{`${label}`}</Text></option>
-        {options && options.map((option) => (
+        {options.map((option) => (
             <option key={option.id} value={option.id || option.ID} label={option["AD-SOYAD"] || option.name} >{option["AD-SOYAD"]  || option.name}</option>
         ))}
         </Select>
