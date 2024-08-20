cdef5a0797eb6e01a45e0d26efb6c4238725575b
Oguz Alp Saglam <oguzalp7@github.com>
2024-08-20 17:07:36 +0300

    api client url changed

--- src/apiClient.jsx
+++ src/apiClient.jsx
@@ -3,8 +3,8 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    //baseURL: process.env.NEXT_PUBLIC_API_URL,
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    //baseURL: "http://127.0.0.1:8000",
     withCredentials: true
 });
 
