2210aaf0bc9835e5e398a0feebfc474c020281fb
Oguz Alp Saglam <oguzalp7@github.com>
2024-08-20 17:34:15 +0300

    trying to fix bugs on production:0.1

--- src/components/dropdown.component.jsx
+++ src/components/dropdown.component.jsx
@@ -21,7 +21,7 @@ const ChakraDropdown = ({options, label, initialValue, value, onSelect}) => {
     <Box>
         <Select value={selectedValue} onChange={handleChange}>
         <option key={-1} value={initialValue} label={`${label}`}><Text>{`${label}`}</Text></option>
-        {options.map((option) => (
+        {options && options.map((option) => (
             <option key={option.id} value={option.id || option.ID} label={option["AD-SOYAD"] || option.name} >{option["AD-SOYAD"]  || option.name}</option>
         ))}
         </Select>
