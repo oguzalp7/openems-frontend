abbd6b962cdf33f70f6ace421cec00cf29b5b86b
Oguz Alp Saglam <oguzalp7@github.com>
2025-12-18 10:56:57 +0300

    fix analysis page

--- src/components/dropdown.component.jsx
+++ src/components/dropdown.component.jsx
@@ -1,36 +1,36 @@
- "use client"
+"use client"
 
 import { Box, Select, Text } from '@chakra-ui/react'
 import React, { useEffect } from 'react'
 
-const ChakraDropdown = ({options, label, initialValue, value, onSelect}) => {
+const ChakraDropdown = ({ options, label, initialValue, value, onSelect }) => {
 
   const [selectedValue, setSelectedValue] = React.useState(value || initialValue);
 
   useEffect(() => {
-      setSelectedValue(value || initialValue);
+    setSelectedValue(value || initialValue);
   }, [value, initialValue]);
 
   const handleChange = (e) => {
-      const selectedId = e.target.value;
-      //console.log(selectedId)
-      onSelect(selectedId);
+    const selectedId = e.target.value;
+    //console.log(selectedId)
+    onSelect(selectedId);
   }
 
   return (
     <Box>
-        <Select value={selectedValue} onChange={handleChange}>
-        <option value={initialValue} label={`${label}`}><Text>{`${label}`}</Text></option>
+      <Select value={selectedValue} onChange={handleChange}>
+        <option key={-1} value={initialValue} label={`${label}`}><Text>{`${label}`}</Text></option>
         <>
-        {
-          
-          options.map((option) => (
-            <option key={option.id} value={option.id || option.ID} label={option["AD-SOYAD"] || option.name} >{option["AD-SOYAD"]  || option.name}</option>
-          ))
-         
-        }
+          {
+
+            options.map((option) => (
+              <option key={option.id} value={option.id || option.ID} label={option["AD-SOYAD"] || option.name} >{option["AD-SOYAD"] || option.name}</option>
+            ))
+
+          }
         </>
-        </Select>
+      </Select>
     </Box>
   )
 }
